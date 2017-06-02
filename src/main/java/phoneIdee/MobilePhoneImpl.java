df62c16e5f9bcdbcd86354251d98fd4e2f38fb85
Mohammad Sawaya <en.mas@hotmail.com>
2017-06-02 18:55:07 +0200

    Mobile phone

--- src/main/java/phoneIdee/MobilePhoneImpl.java
+++ src/main/java/phoneIdee/MobilePhoneImpl.java
@@ -7,7 +7,7 @@ public class MobilePhoneImpl extends PhoneImpl implements MobilePhone {
 
     @Override
     public void sendSMS(String sms, String num) {
-
+        System.out.println("the msg: "+sms+"\n form : "+num);
     }
 
     @Override
